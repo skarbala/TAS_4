7d3d803ae61c5a5365193b1be34319df5aee7610
martin <skarbala.martin@gmail.com>
2023-10-12 19:17:43 +0200

    adds materials from lecture

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("setCookiesContext", () => {
+    cy.log("Set cookies - agreed")
+    cy.setCookie("__kwc_agreed", "true")
+    cy.setCookie("__kwc_settings", "%7B%22marketing%22%3Atrue%2C%22analytics%22%3Atrue%7D")
+})
+
+Cypress.Commands.add("setBookingButton", () => {
+    cy.log("Uncheck booking extension button")
+    localStorage.setItem("bookingcom_extension_default", "false")
+})
